d99b1e47941221993c86015717abc0da81a1593a
Getkey <getkey@getkey.eu>
2016-12-03 17:51:50 +0100

    Add more tests

--- tests/proto.js
+++ tests/proto.js
@@ -13,6 +13,8 @@ test('register', t => {
 		res = proto.register.deserialize(buf);
 
 	t.deepEqual(userData, res);
+
+	t.is(proto.getSerializator(buf), proto.register);
 });
 
 test('addSlaves', t => {
@@ -37,4 +39,157 @@ test('addSlaves', t => {
 		res = proto.addSlaves.deserialize(buf);
 
 	t.deepEqual(slaves, res);
+
+	t.is(proto.getSerializator(buf), proto.addSlaves);
+});
+
+test('removeSlaves', t => {
+	let slaveIds = [12, 367, 666, 42, 9892, 2147483647, 0],
+		buf = proto.removeSlaves.serialize(slaveIds),
+		res = proto.removeSlaves.deserialize(buf);
+
+	t.deepEqual(slaveIds, res);
+
+	t.is(proto.getSerializator(buf), proto.removeSlaves);
+});
+
+
+let sdp = `v=0
+o=mozilla...THIS_IS_SDPARTA-50.0.2 2216985128585860715 0 IN IP4 0.0.0.0
+s=-
+t=0 0
+a=fingerprint:sha-256 D1:56:02:AD:4B:0C:E5:DE:03:DE:66:B9:48:FF:2C:89:99:4C:E7:83:FA:1B:CB:3A:1C:30:FF:A5:3D:20:4F:38
+a=ice-options:trickle
+a=msid-semantic:WMS *
+m=application 9 DTLS/SCTP 5000
+c=IN IP4 0.0.0.0
+a=sendrecv
+a=ice-pwd:31fbcb74d9a82304ed0f09d792140bb7
+a=ice-ufrag:6562eea9
+a=mid:sdparta_0
+a=sctpmap:5000 webrtc-datachannel 256
+a=setup:actpass
+a=ssrc:2503778014 cname:{b231cbd5-e93d-4501-94b7-f1cc8e87a0b7}`;
+
+test('offerToSlave', t => {
+	let id = 2147483647,
+		buf = proto.offerToSlave.serialize(id, sdp),
+		res = proto.offerToSlave.deserialize(buf);
+
+	t.is(res.id, id);
+	t.is(res.sdp, sdp);
+
+	t.is(proto.getSerializator(buf), proto.offerToSlave);
+});
+
+test('offerFromClient', t => {
+	let id = 0,
+		buf = proto.offerFromClient.serialize(id, sdp),
+		res = proto.offerFromClient.deserialize(buf);
+
+	t.is(res.id, id);
+	t.is(res.sdp, sdp);
+
+	t.is(proto.getSerializator(buf), proto.offerFromClient);
+});
+
+test('answerToClient', t => {
+	let id = 1,
+		buf = proto.answerToClient.serialize(id, sdp),
+		res = proto.answerToClient.deserialize(buf);
+
+	t.is(res.id, id);
+	t.is(res.sdp, sdp);
+
+	t.is(proto.getSerializator(buf), proto.answerToClient);
+});
+
+test('answerFromSlave', t => {
+	let id = 67,
+		buf = proto.answerFromSlave.serialize(id, sdp),
+		res = proto.answerFromSlave.deserialize(buf);
+
+	t.is(res.id, id);
+	t.is(res.sdp, sdp);
+
+	t.is(proto.getSerializator(buf), proto.answerFromSlave);
+});
+
+
+let candidate = {
+	candidate: "candidate:0 1 UDP 2122252543 192.168.0.22 52840 typ host",
+	sdpMLineIndex: 0,
+	sdpMid: "sdparta_0"
+};
+
+test('iceCandidateToSlave', t => {
+	let id = 0,
+		buf1 = proto.iceCandidateToSlave.serialize(id, candidate),
+		res1 = proto.iceCandidateToSlave.deserialize(buf1),
+		buf2 = proto.iceCandidateToSlave.serialize(id, null),
+		res2 = proto.iceCandidateToSlave.deserialize(buf2);
+
+	t.is(id, res1.id);
+	t.is(candidate.candidate, res1.candidate);
+	t.is(candidate.sdpMid, res1.sdpMid);
+	t.is(candidate.sdpMLineIndex, res1.sdpMLineIndex);
+
+	t.is(id, res2.id);
+	t.is(null, res2.candidate);
+
+	t.is(proto.getSerializator(buf1), proto.iceCandidateToSlave);
+});
+
+test('iceCandidateFromClient', t => {
+	let id = 2147483647,
+		buf1 = proto.iceCandidateFromClient.serialize(id, candidate),
+		res1 = proto.iceCandidateFromClient.deserialize(buf1),
+		buf2 = proto.iceCandidateFromClient.serialize(id, null),
+		res2 = proto.iceCandidateFromClient.deserialize(buf2);
+
+	t.is(id, res1.id);
+	t.is(candidate.candidate, res1.candidate);
+	t.is(candidate.sdpMid, res1.sdpMid);
+	t.is(candidate.sdpMLineIndex, res1.sdpMLineIndex);
+
+	t.is(id, res2.id);
+	t.is(null, res2.candidate);
+
+	t.is(proto.getSerializator(buf1), proto.iceCandidateFromClient);
+});
+
+test('iceCandidateToClient', t => {
+	let id = 8789,
+		buf1 = proto.iceCandidateToClient.serialize(id, candidate),
+		res1 = proto.iceCandidateToClient.deserialize(buf1),
+		buf2 = proto.iceCandidateToClient.serialize(id, null),
+		res2 = proto.iceCandidateToClient.deserialize(buf2);
+
+	t.is(id, res1.id);
+	t.is(candidate.candidate, res1.candidate);
+	t.is(candidate.sdpMid, res1.sdpMid);
+	t.is(candidate.sdpMLineIndex, res1.sdpMLineIndex);
+
+	t.is(id, res2.id);
+	t.is(null, res2.candidate);
+
+	t.is(proto.getSerializator(buf1), proto.iceCandidateToClient);
+});
+
+test('iceCandidateFromSlave', t => {
+	let id = 123,
+		buf1 = proto.iceCandidateFromSlave.serialize(id, candidate),
+		res1 = proto.iceCandidateFromSlave.deserialize(buf1),
+		buf2 = proto.iceCandidateFromSlave.serialize(id, null),
+		res2 = proto.iceCandidateFromSlave.deserialize(buf2);
+
+	t.is(id, res1.id);
+	t.is(candidate.candidate, res1.candidate);
+	t.is(candidate.sdpMid, res1.sdpMid);
+	t.is(candidate.sdpMLineIndex, res1.sdpMLineIndex);
+
+	t.is(id, res2.id);
+	t.is(null, res2.candidate);
+
+	t.is(proto.getSerializator(buf1), proto.iceCandidateFromSlave);
 });
