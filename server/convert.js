f86834bca8cd878a50feff76162d3cca369296ce
Will Stott <willstott101@gmail.com>
2020-04-01 20:59:25 +0100

    Fix nodejs Buffer warning

--- server/convert.js
+++ server/convert.js
@@ -1,12 +1,12 @@
 export function stringToBuffer(string) {
-	let buf = new Buffer(string, 'utf8');
+	let buf = Buffer.from(string, 'utf8');
 	return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength);
 }
 
 export function bufferToString(arrayBuffer) {
 	let StringDecoder = require('string_decoder').StringDecoder,
 		decoder = new StringDecoder('utf8'),
-		tmpBuf = new Buffer(arrayBuffer);
+		tmpBuf = Buffer.from(arrayBuffer);
 	return decoder.write(tmpBuf);
 }
 
